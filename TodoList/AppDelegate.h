11b8e898f3c214459bcdafdef51f671109ea8fc3
jivetestuser <jivetestuser@yahoo.com>
2014-04-20 10:35:03 -0700

    Initial commit

---
+++ TodoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TodoList
+//
+//  Created by Prashanth Govindaraj on 4/20/14.
+//  Copyright (c) 2014 Prashanth Govindaraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TodoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TodoList
+//
+//  Created by Prashanth Govindaraj on 4/20/14.
+//  Copyright (c) 2014 Prashanth Govindaraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
